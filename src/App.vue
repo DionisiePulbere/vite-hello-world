09ba610c70777560f33799cad0d10741dedfe5a8
DionisiePulbere <dionisiepulbere@gmail.com>
2024-04-10 17:13:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppJumbo from './components/AppJumbo.vue';
+
+  export default {
+    components: {
+      AppJumbo
+    }
+  }
+ 
+</script>
+
+<template>
+  <!-- html -->
+   <AppJumbo></AppJumbo>
+</template>
+
+<style>
+  /* css */
+  
+</style>
---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,31 @@
+<script>
+import AppTitle from './AppTitle.vue';
+  export default {
+    name: 'AppJumbo',
+    components:{
+        AppTitle
+    }
+
+  }
+</script>
+
+<template>
+  <div class="container">
+    <AppTitle></AppTitle>
+  </div>
+</template>
+
+<style scoped>
+.container{
+    width: 100%;
+    height: 400px;
+    background-image: url(https://www.serverplan.com/blog/wp-content/uploads/2022/07/colore-google.jpg);
+    background-size: cover;
+    background-position: center;
+}
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
